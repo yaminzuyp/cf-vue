121c033cba36bd5da830ba5ae263f9adcc3c9879
yaminzuyp <yzainiyah@gmail.com>
2025-06-27 12:44:55 +0700

    setting db

--- api/index.js
+++ api/index.js
@@ -1,17 +1,42 @@
-import { Hono } from 'hono';
+import { Hono } from 'hono'
 
-const app = new Hono();
+const app = new Hono<{ Bindings: Env }>()
 
-app.get('/api', (c) => {
-  return c.text('hello');
-})
-
-app.get('/api/users', async (c) => {
-  let { results } = await c.env.DB.prepare("SELECT * FROM users").all()
+app.get('/api/chats', async (c) => {
+  const { results } = await c.env.DB.prepare("SELECT * FROM chats").all()
   return c.json(results)
 })
 
-// fallback ke frontend
-app.get('*', (c) => c.env.ASSETS.fetch(c.req.raw));
+app.post('/api/chats', async (c) => {
+  const { user_id, message } = await c.req.json()
+  await c.env.DB.prepare(`
+    INSERT INTO chats(id, user_id, message) VALUES (?, ?, ?)`
+  ).bind(crypto.randomUUID(), user_id, message).run()
 
-export default app;
+  return c.json({message: 'ok'})
+})
+
+app.put('/api/chats/:id', async (c) => {
+  const { id } = c.req.param()
+  const { user_id, message } = await c.req.json()
+
+  await c.env.DB.prepare(`
+    UPDATE chats SET user_id = ?, message = ? WHERE id = ?
+  `).bind(user_id, message, id).run()
+
+  return c.json({message: 'ok'})
+})
+
+app.delete('/api/chats/:id', async (c) => {
+  const { id } = c.req.param()
+  await c.env.CHAT_DB.prepare(`
+    DELETE FROM chats WHERE id = ?
+  `).bind(id).run()
+
+  return c.json({message: 'ok'})
+})
+
+// untuk fallback ke frontend
+app.get('*', async (c) => await c.env.ASSETS.fetch(c.req.raw))
+
+export default app
--- migrations/0001_create_users.sql
+++ migrations/0001_create_users.sql
@@ -1,8 +1,13 @@
--- Migration number: 0001 	 2025-06-21T08:54:20.430Z
+-- Migration number: 0001 	 2025-04-21T10:54:23.652Z
+
 CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id TEXT PRIMARY KEY,
     name TEXT NOT NULL,
-    address TEXT
+    avatar TEXT
 );
 
-INSERT INTO users(name, address) VALUES ('Andre', 'Purwosari'), ('Andian', 'Sukorejo'), ('Ana', 'Pandaaan');
+CREATE TABLE IF NOT EXISTS chats (
+    id TEXT PRIMARY KEY,
+    user_id TEXT NOT NULL,
+    message TEXT NOT NULL
+);
