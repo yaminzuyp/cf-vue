0f631eb1fea2d3dd7be96f30792891c8240a7c99
yaminzuyp <yzainiyah@gmail.com>
2025-06-27 13:29:26 +0700

    aetting api

--- api/index.js
+++ api/index.js
@@ -2,35 +2,40 @@ import { Hono } from 'hono'
 
 const app = new Hono<{ Bindings: Env }>
 
-app.get('/api/chats', async (c) => {
-  const { results } = await c.env.DB.prepare("SELECT * FROM chats").all()
+app.get('/api', (c) => {
+  return c.text('hi');
+})
+
+
+app.get('/api/users', async (c) => {
+  const { results } = await c.env.DB.prepare("SELECT * FROM users").all()
   return c.json(results)
 })
 
-app.post('/api/chats', async (c) => {
+app.post('/api/users', async (c) => {
   const { user_id, message } = await c.req.json()
   await c.env.DB.prepare(`
-    INSERT INTO chats(id, user_id, message) VALUES (?, ?, ?)`
+    INSERT INTO users(id, user_id, message) VALUES (?, ?, ?)`
   ).bind(crypto.randomUUID(), user_id, message).run()
 
   return c.json({message: 'ok'})
 })
 
-app.put('/api/chats/:id', async (c) => {
+app.put('/api/users/:id', async (c) => {
   const { id } = c.req.param()
   const { user_id, message } = await c.req.json()
 
   await c.env.DB.prepare(`
-    UPDATE chats SET user_id = ?, message = ? WHERE id = ?
+    UPDATE usersSET user_id = ?, message = ? WHERE id = ?
   `).bind(user_id, message, id).run()
 
   return c.json({message: 'ok'})
 })
 
-app.delete('/api/chats/:id', async (c) => {
+app.delete('/api/users/:id', async (c) => {
   const { id } = c.req.param()
   await c.env.CHAT_DB.prepare(`
-    DELETE FROM chats WHERE id = ?
+    DELETE FROM users WHERE id = ?
   `).bind(id).run()
 
   return c.json({message: 'ok'})
