403433f855d4cf2871c108101b7e4afdd76c9642
yaminzuyp <yzainiyah@gmail.com>
2025-06-28 20:26:45 +0700

    setting db

--- migrations/0001_create_users.sql
+++ migrations/0001_create_users.sql
@@ -1,14 +1,9 @@
 -- Migration number: 0001 	 2025-06-21T08:54:20.430Z
 
-CREATE TABLE IF NOT EXISTS users (
-    id TEXT PRIMARY KEY,
-    name TEXT NOT NULL,
-    avatar TEXT
-);
-
 CREATE TABLE IF NOT EXISTS chats (
     id TEXT PRIMARY KEY,
     user_id TEXT NOT NULL,
-    message TEXT NOT NULL
+    message TEXT NOT NULL,
+    timestamp TEXT NOT NULL
 );
 
