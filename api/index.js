fdf95bd8ad2528ab43aebf94d62ea84e06e7dd92
yaminzuyp <yzainiyah@gmail.com>
2025-06-28 21:42:57 +0700

    setting db

---
+++ api/index.js
@@ -0,0 +1,47 @@
+import { Hono } from 'hono'
+
+const app = new Hono<{ Bindings: Env }>
+
+app.get('/api', (c) => {
+  return c.text('hi');
+})
+
+
+app.get('/api/users', async (c) => {
+  const { results } = await c.env.DB.prepare("SELECT * FROM users").all()
+  return c.json(results)
+})
+
+app.post('/api/users', async (c) => {
+  const { user_id, message } = await c.req.json()
+  await c.env.DB.prepare(`
+    INSERT INTO users(id, user_id, message) VALUES (?, ?, ?)`
+  ).bind(crypto.randomUUID(), user_id, message).run()
+
+  return c.json({message: 'ok'})
+})
+
+app.put('/api/users/:id', async (c) => {
+  const { id } = c.req.param()
+  const { user_id, message } = await c.req.json()
+
+  await c.env.DB.prepare(`
+    UPDATE usersSET user_id = ?, message = ? WHERE id = ?
+  `).bind(user_id, message, id).run()
+
+  return c.json({message: 'ok'})
+})
+
+app.delete('/api/users/:id', async (c) => {
+  const { id } = c.req.param()
+  await c.env.CHAT_DB.prepare(`
+    DELETE FROM users WHERE id = ?
+  `).bind(id).run()
+
+  return c.json({message: 'ok'})
+})
+
+// untuk fallback ke frontend
+app.get('*', async (c) => await c.env.ASSETS.fetch(c.req.raw))
+
+export default app
